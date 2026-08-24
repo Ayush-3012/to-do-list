412f78a9cc81988a141378003a56b2f50b020ff9
Ayush Kumar <diveshsrivastava31@gmail.com>
2026-08-24 16:31:06 +0530

    add new frontend deployed url to allow cors origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173"]
+const allowedOrigins = ["http://localhost:5173", "https://to-do-list-assignment-ayush.vercel.app"]
 
 app.use(cors({
     origin: (origin, callback) => {
@@ -19,6 +19,8 @@ app.use(cors({
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => res.send("Backend Is Running..."))
 
 import taskRouter from "./routes/task.route.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
